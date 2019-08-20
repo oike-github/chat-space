b449c09af6f2093a60f0eb14073190f073464015
oike-github <chikakoike.work@gmail.com>
2019-08-20 15:46:40 +0900

    Add groups and messages to routing
    Added groups and messages to routing.
    Added edit.html and new .html under view / group

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update] do
+    resources :messages onry: [:index, :create]
   end
