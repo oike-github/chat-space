1bf893ffd6356309a992fc32a64ef1f62931eb8c
oike-github <chikakoike.work@gmail.com>
2019-08-21 14:59:46 +0900

    Added processing to new and create in groups_controller
    Added processing to new and create in groups_controller.
    
    Added validation to group model
    
    Change the view / groups / new.html.haml form to form_for

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,33 @@
 class GroupsController < ApplicationController
   def new
-    
+    # Groupモデルのインスタンスを作成する
+    @group = Group.new
+    # Groupモデルのインスタンスのuserに現在ログインしているユーザーを追加する
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      # groupテーブルへの登録が成功した場合
+      # root_pathへオプション(通知用メッセージ)付きでリダイレクト
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      # groupテーブルへの登録が失敗した場合
+      # newアクションのviewのみを表示
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    # new.html.hamlからの値（グループ名、）
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 end
