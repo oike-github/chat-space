c407125520abe5ffffa1bd45f9031af6bb3cb7e7
oike-github <chikakoike.work@gmail.com>
2019-08-23 03:13:20 +0900

    Change root path
    Changed root path from messages to groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'
+  root to: 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, onry: [:new, :create, :edit, :update] do
     resources :messages, onry: [:index, :create]
